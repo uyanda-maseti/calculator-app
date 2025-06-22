9b483d4d1643dfa14afd5abd89fa8b3363d0474a
uyanda-maseti <masetibabalo2@gmail.com>
2025-06-22 21:31:32 +0200

    Create a function to handle event when the digit button is clicked

---
+++ script.js
@@ -0,0 +1,6 @@
+function digitBtnPressed(button){
+    var currentValue = document.getElementById('inputBox').value
+    
+    document.getElementById('inputBox').value = currentValue + button;
+
+}
