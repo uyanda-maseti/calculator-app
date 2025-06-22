887be1af8fcdc393f75fda84d59b783f48cb99ae
uyanda-maseti <masetibabalo2@gmail.com>
2025-06-23 00:26:53 +0200

    Add event handler when equals button is pressed Add classes for styling on index.html Add styles for the calculator project on style.css

--- script.js
+++ script.js
@@ -17,9 +17,33 @@ function btnACPressed(){
     document.getElementById('inputBox').value = 0
     newLine = true
 }
-
+// Event handler for operator buttons
 function operatorBtnPressed(operator){
     currentOperator = operator
     value1 = parseInt(document.getElementById('inputBox').value)
+    newLine = true
+}
+
+// Event handle for equals button
+function equalsBtnPressed(){
+    var value2 = parseInt(document.getElementById('inputBox').value)
+    var finalTotal
+    switch(currentOperator){
+        case '+':
+            finalTotal = value1 + value2
+            break
+        case '-':
+            finalTotal = value1 - value2
+            break
+        case 'x':
+            finalTotal = value1 * value2
+            break
+        case '/':
+            finalTotal = value1 / value2
+
+    }
+    document.getElementById('inputBox').value = finalTotal
+    
+    value1 = 0
     newLine = true
 }
