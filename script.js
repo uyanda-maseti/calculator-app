d371a10e509060ef207ffa1ea43bcaf5cf9d93a6
uyanda-maseti <masetibabalo2@gmail.com>
2025-06-22 22:20:16 +0200

    The first digit should replace the zero in the input

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+var newLine = true
+
 function digitBtnPressed(button){
-    var currentValue = document.getElementById('inputBox').value
-    
-    document.getElementById('inputBox').value = currentValue + button;
+    if(newLine){
+        document.getElementById('inputBox').value = button
+        newLine = false
+    }else{
+        var currentValue = document.getElementById('inputBox').value
+        document.getElementById('inputBox').value = currentValue + button;
+    }
 
 }
